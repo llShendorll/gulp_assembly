83c01d5879d629165fac21f48d8a3a55f37e343d
polyakovas <webshendor@gmail.com>
2016-09-25 19:44:06 +0300

    assembly-gulp

---
+++ gulp/tasks/sprite.png.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function() {
+  $.gulp.task('sprite:png', function() {
+    var spriteData = $.gulp.src('./source/sprite/*.png')
+        .pipe($.gp.spritesmith({
+          imgName: 'sprite.png',
+          cssName: 'sprite.scss',
+          padding: 20,
+          algorithm: 'binary-tree'
+        }));
+
+    var imgStream = spriteData.img
+        .pipe($.gulp.dest($.config.root + '/assets/img'));
+
+    var cssStream = spriteData.css
+        .pipe($.gulp.dest($.config.root + '/assets/css'));
+
+    return ($.merge(imgStream, cssStream));
+  })
+};
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+global.$ = {
+    package: require('./package.json'),
+    config: require('./gulp/config'),
+    path: {
+        task: require('./gulp/paths/tasks.js'),
+        jsFoundation: require('./gulp/paths/js.foundation.js'),
+        cssFoundation: require('./gulp/paths/css.foundation.js'),
+        app: require('./gulp/paths/app.js')
+    },
+    merge: require('merge-stream'),
+    gulp: require('gulp'),
+    rimraf: require('rimraf'),
+    browserSync: require('browser-sync').create(),
+    gp: require('gulp-load-plugins')()
+};
+
+$.path.task.forEach(function(taskPath) {
+  require(taskPath)();
+});
+
+$.gulp.task('default', $.gulp.series(
+  'clean',
+  $.gulp.parallel(
+    'sass',
+    'pug',
+    'js:foundation',
+    'js:process',
+    'copy:image',
+    'copy:fonts',
+    'css:foundation',
+    'sprite:svg',
+    'sprite:png'
+  ),
+  $.gulp.parallel(
+    'watch',
+    'serve'
+  )
+));
