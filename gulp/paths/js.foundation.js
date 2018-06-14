9c593bcc06ff706942751dde799ed1c7f3f18dfe
System Administrator <root@Andrejs-iMac.local>
2018-06-14 14:29:44 +0300

    обновил версии

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -1,6 +1,5 @@
 'use strict';
 
 module.exports = [
-    './bower_components/jquery/dist/jquery.min.js',
-    //'./node_modules/jquery-ui-dist/jquery-ui.min.js',
+    './node_modules/jquery/dist/jquery.min.js'
 ];
