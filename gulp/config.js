f76503249292f6968df0371749c73be70c881356
 Shendor <shendor@MacBook-Pro-Shendor.local>
2021-10-25 01:29:44 +0300

    update package

--- gulp/config.js
+++ gulp/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   root: './build',
 
-  autoprefixerConfig: ['last 16 version', '> 1%', 'ie 8', 'ie 9', 'Opera 12.1']
+  autoprefixerConfig: ['last 16 version', '> 1%', 'ie 8', 'ie 9', 'ie 10', 'Opera 12.1']
 };
--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,11 +1,12 @@
 'use strict';
+var sass = require('gulp-sass')(require('sass'));
 
 module.exports = function() {
   $.gulp.task('sass', function() {
     return $.gulp.src('./source/style/app.scss')
       .pipe($.gp.sourcemaps.init())
-      .pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' }))
-      .pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
+      .pipe(sass()).on('error', $.gp.notify.onError({ title: 'Style' }))
+      .pipe($.gp.autoprefixer({ overrideBrowserslist: $.config.autoprefixerConfig }))
       .pipe($.gp.sourcemaps.write())
       .pipe($.gulp.dest($.config.root + '/assets/css'))
       .pipe($.browserSync.stream());
